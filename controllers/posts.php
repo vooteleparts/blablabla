fe6aa38a0490b58051b2d0adf67abe4614759179
Vootele Parts <vootele.parts@khk.ee>
2013-10-04 22:00:16 +0300

    Add single post viewing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -5,4 +5,8 @@ class posts extends Controller{
 	function index(){
 		$this->posts = get_all("SELECT * FROM post");
 	}
+	function view(){
+		$post_id = $this->params[0];
+		$this->post = get_one("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+	}
 }
--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -7,8 +7,9 @@
 
 
 <div class="span8">
+
 <?foreach($posts as $post):?>
-	<h1><?=$post['post_subject']?></h1>
+	<h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
 	<p><?=$post['post_text']?></p>
 	<div>
 		<span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
